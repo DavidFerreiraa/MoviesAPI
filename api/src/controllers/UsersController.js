46c40914145084dd8a890edfdd6d80f4f46dfecc
David <dfl8@aluno.ifal.edu.br>
2024-03-13 08:13:10 -0300

    feat: add create user metod in usersController

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,6 +1,27 @@
+const knex = require("../database/knex");
+const { hash } = require("bcryptjs");
+
 class UsersController {
     async create(request, response) {
-        
+        const { name, email, password, avatar } = request.body;
+
+        const userExists = await knex.select().from("users").where("email", email);
+        console.log(userExists)
+
+        if (userExists.length) {
+            throw new Error("This e-mail is already used.");
+        }
+
+        const hashedPassword = await hash(password, 8);
+
+        await knex("users").insert({
+            name,
+            email,
+            password: hashedPassword,
+            avatar
+        })
+
+        return response.json()
     }
 }
 
--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const usersController = require("../controllers/UsersController.js");
+const UsersController = require("../controllers/UsersController.js");
 
 const usersRoutes = Router();
+const usersController = new UsersController();
 
-usersRoutes.get("/", usersController)
+usersRoutes.post("/", usersController.create)
 
 module.exports = usersRoutes;
